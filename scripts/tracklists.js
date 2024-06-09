d025f029c01ad68956fc3f8d922dd9ae62208642
AlmaDyne <123851079+AlmaDyne@users.noreply.github.com>
2024-06-09 07:52:55 +0300

    Add files via upload

--- scripts/tracklists.js
+++ scripts/tracklists.js
@@ -15,56 +15,56 @@ tracklistsMapData.set('be2c049d-11d8-4060-8595-c05cb2e85930', {
             order: 1,
             artist: 'Zmicier M.',
             title: 'Cold Lights (Metal Mix by ATD)',
-            src: 'https://almadyne.github.io/audio-player/music/Experiments_and_Parodies/1._Zmicier_M._-_Cold_Lights_(Metal_Mix_by_ATD).mp3'
+            src: 'music/Experiments_and_Parodies/1._Zmicier_M._-_Cold_Lights_(Metal_Mix_by_ATD).mp3'
         },
         {
             id: 'e777ad8e-7f5e-43d6-b28a-ad7f6bbdfc1e',
             order: 2,
             artist: 'Zmicier M.',
             title: 'Silent Minds (feat. ATD)',
-            src: 'https://almadyne.github.io/audio-player/music/Experiments_and_Parodies/2._Zmicier_M._-_Silent_Minds_(feat._ATD).mp3'
+            src: 'music/Experiments_and_Parodies/2._Zmicier_M._-_Silent_Minds_(feat._ATD).mp3'
         },
         {
             id: '785b0f2b-f5a2-42f4-a7ea-00ab110d1cef',
             order: 3,
             artist: 'Zmicier M.',
             title: 'Classical TechnoDub (feat. ATD)',
-            src: 'https://almadyne.github.io/audio-player/music/Experiments_and_Parodies/3._Zmicier_M._-_Classical_TechnoDub_(feat._ATD).mp3'
+            src: 'music/Experiments_and_Parodies/3._Zmicier_M._-_Classical_TechnoDub_(feat._ATD).mp3'
         },
         {
             id: 'b79324f3-2ea2-49ee-b6d3-aa80151126fd',
             order: 4,
             artist: 'Zmicier M.',
             title: 'First Snow (feat. ATD)',
-            src: 'https://almadyne.github.io/audio-player/music/Experiments_and_Parodies/4._Zmicier_M._-_First_Snow_(feat._ATD).mp3'
+            src: 'music/Experiments_and_Parodies/4._Zmicier_M._-_First_Snow_(feat._ATD).mp3'
         },
         {
             id: '558ca586-1730-4847-9cbe-77d2a200aa62',
             order: 5,
             artist: 'Extra-MEGA Internet Band Collaboration with Limited Capabilities, But Unlimited Inspiration',
             title: 'New Year 2020 Experiment (Super Apocalyptic Remix and Extremely Cool Job by ATD)',
-            src: 'https://almadyne.github.io/audio-player/music/Experiments_and_Parodies/5._Extra-MEGA_Internet_Band_Collaboration_-_New_Year_2020_Experiment_(Super_Apocalyptic_Remix_and_Extremely_Cool_Job_by_ATD).mp3'
+            src: 'music/Experiments_and_Parodies/5._Extra-MEGA_Internet_Band_Collaboration_-_New_Year_2020_Experiment_(Super_Apocalyptic_Remix_and_Extremely_Cool_Job_by_ATD).mp3'
         },
         {
             id: '8a127402-f1a8-473c-8de5-b8e13f059598',
             order: 6,
             artist: 'Zmicier M.',
             title: 'Harpsichords Attack! (feat. ATD)',
-            src: 'https://almadyne.github.io/audio-player/music/Experiments_and_Parodies/6._Zmicier_M._-_Harpsichords_Attack!_(feat._ATD).mp3'
+            src: 'music/Experiments_and_Parodies/6._Zmicier_M._-_Harpsichords_Attack!_(feat._ATD).mp3'
         },
         {
             id: '7ea2494c-68f9-4c7a-a91a-81e10e433ed5',
             order: 7,
             artist: 'Zmicier M.',
             title: 'Thousand Miles (of Drinking) (feat. ATD)',
-            src: 'https://almadyne.github.io/audio-player/music/Experiments_and_Parodies/7._Zmicier_M._-_Thousand_Miles_(of_Drinking)_(feat._ATD).mp3'
+            src: 'music/Experiments_and_Parodies/7._Zmicier_M._-_Thousand_Miles_(of_Drinking)_(feat._ATD).mp3'
         },
         {
             id: 'dfc64e39-3001-42c1-affa-9000b471b153',
             order: 8,
             artist: 'Dmitry Miroshkin & Anthony Driverson',
             title: 'Raindrops (Guitar Mix)',
-            src: 'https://almadyne.github.io/audio-player/music/Experiments_and_Parodies/8._Dmitry_Miroshkin_&_Anthony_Driverson_-_Raindrops_(Guitar_Mix).mp3'
+            src: 'music/Experiments_and_Parodies/8._Dmitry_Miroshkin_&_Anthony_Driverson_-_Raindrops_(Guitar_Mix).mp3'
         }
     )
 });
